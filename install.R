27c582a35380e32d7fa1c89c782d6d9514611883
cjwomack <39180413+cjwomack@users.noreply.github.com>
2020-07-19 20:43:34 +1000

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 ## try http:// if https:// URLs are not supported
+install.packages("XML")
 if (!requireNamespace("BiocManager", quietly=TRUE)) install.packages("BiocManager")
 BiocManager::install("InterMineR")
